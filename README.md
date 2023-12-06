009fc17bedf9421bfbe791c6f8dd8d8fb46d3fe2
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-12-06 16:08:44 -0600

    changed name and ecr deployment script

--- README.md
+++ README.md
@@ -86,8 +86,8 @@ Everything on there will be allowed to be got. Example:
 background: url('http://localhost:8080/waw.png');
 
 ## For Docker
-1) Run `docker build -t react-docker-aws:latest .` to build project
-2) Run `docker run -p 3000:80 react-docker-aws:latest` to run on port 3000
+1) Run `docker build -t trtlmail-web:latest .` to build project
+2) Run `docker run -p 3000:80 trtlmail-web:latest` to run on port 3000
 
 ## For enabling https
 1) Install certificate in your browser as a pfx if not already done
--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,12 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 
 echo "Build Steps:"
 echo "building image..."
-docker build -t 514832027284.dkr.ecr.us-east-1.amazonaws.com/trtlmail-web:latest .
+docker build -t trtlmail-web .
+docker tag trtlmail-web:latest 514832027284.dkr.ecr.us-east-1.amazonaws.com/trtlmail-web:latest
 
 echo "updating AWS ECS service..."
 #aws ecs update-service --cluster <cluster-name> --service <service-name> --force-new-deployment
 
 echo "Post-Build steps:"
 echo "pushing image to AWS ECR"
-docker push --all-tags 514832027284.dkr.ecr.us-east-1.amazonaws.com/trtlmail-web
+docker push 514832027284.dkr.ecr.us-east-1.amazonaws.com/trtlmail-web:latest
