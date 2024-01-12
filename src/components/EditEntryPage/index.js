ce2d3ea5a0908e7e60270fcf477e8462713d7c7c
jackmuva <33766184+jackmuva@users.noreply.github.com>
2024-01-12 13:58:59 -0600

    using https rest endpoint

--- src/components/EditEntryPage/index.js
+++ src/components/EditEntryPage/index.js
@@ -29,7 +29,7 @@ function EditEntryPage(){
                     class: ImageTool,
                     config: {
                         endpoints: {
-                            byFile: 'http://trtlmail-rest.com:5000/api/image/save', // Your backend file uploader endpoint
+                            byFile: 'https://trtlmail-rest.com/api/image/save', // Your backend file uploader endpoint
                             byUrl: '', // Your endpoint that provides uploading by Url
                         },
                         additionalRequestHeaders: {
@@ -40,7 +40,7 @@ function EditEntryPage(){
                 linkTool: {
                     class: LinkTool,
                     config: {
-                        endpoint: 'http://trtlmail-rest.com:5000/api/fetchUrl', // Your backend endpoint for url data fetching,
+                        endpoint: 'https://trtlmail-rest.com/api/fetchUrl', // Your backend endpoint for url data fetching,
                         headers: {
                             'Authorization': 'Bearer ' + sessionStorage.getItem("jwt")
                         }
--- src/components/SeriesInfoPage/EntryPreview/index.js
+++ src/components/SeriesInfoPage/EntryPreview/index.js
@@ -25,7 +25,7 @@ function EntryPreview({entry}){
                     class: ImageTool,
                     config: {
                         endpoints: {
-                            byFile: 'http://trtlmail-rest.com:5000/api/image/save', // Your backend file uploader endpoint
+                            byFile: 'https://trtlmail-rest.com/api/image/save', // Your backend file uploader endpoint
                             byUrl: '', // Your endpoint that provides uploading by Url
                         },
                         additionalRequestHeaders: {
@@ -36,7 +36,7 @@ function EntryPreview({entry}){
                 linkTool: {
                     class: LinkTool,
                     config: {
-                        endpoint: 'http://trtlmail-rest.com:5000/api/fetchUrl', // Your backend endpoint for url data fetching,
+                        endpoint: 'https://trtlmail-rest.com/api/fetchUrl', // Your backend endpoint for url data fetching,
                         headers: {
                             'Authorization': 'Bearer ' + sessionStorage.getItem("jwt")
                         }
