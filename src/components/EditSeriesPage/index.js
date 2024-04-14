9dd9a63ab8f07cbad86954d8f94ba2006330db57
jackmuva <33766184+jackmuva@users.noreply.github.com>
2024-04-14 14:48:40 -0500

    created the sidebar for editing series

--- src/components/EditSeriesPage/index.js
+++ src/components/EditSeriesPage/index.js
@@ -1,8 +1,10 @@
-import {useLocation} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 import EntryApi from "../../api/EntryApi";
 import Entry from "../Entry";
 import SeriesApi from "../../api/SeriesApi";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleLeft, faPenToSquare} from "@fortawesome/free-solid-svg-icons";
 
 function EditSeriesPage(){
     const [entries, setEntries] = useState([]);
@@ -44,14 +46,29 @@ function EditSeriesPage(){
         updateSeries(series);
     };
 
+    let writerUrl = "/writer/" + sessionStorage.getItem("penName");
     if(entries.length === 0){
         return (
             <div>
-                <button onClick={() => createEntry(1)} type="submit"
-                        className="m-4 px-2 py-1 rounded-md text-slate-50 bg-orange-700 hover:bg-orange-800">
-                    Create New Entry
-                </button>
-                <div className="flex items-center md:mx-52">
+                <aside
+                    className="mt-10 fixed top-20 left-0 z-40 w-64 h-screen pt-10 transition-transform -translate-x-full sm:translate-x-0 bg-gradient-to-t from-gray-200">
+                    <div className="h-full px-3 overflow-y-auto">
+                        <ul className="space-y-2 font-medium">
+                            <li className="hover:bg-gray-200 rounded-xl p-4">
+                                <FontAwesomeIcon icon={faPenToSquare}/>
+                                <button className="pl-2 text-xl" onClick={() => createEntry(1)}
+                                        type="submit">
+                                    Create New Entry
+                                </button>
+                            </li>
+                            <li className="hover:bg-gray-200 rounded-xl p-4">
+                                <FontAwesomeIcon icon={faCircleLeft} />
+                                <a className="pl-2 text-xl" href={writerUrl}>Back to Series Page</a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+                <div className="pl-20 flex items-center md:mx-52">
                     <div className="flex flex-col my-3 space-y-0 mx-6 min-w-full text-center items-center">
                         <h3 className="my-8 font-sans text-3xl"> No Entries Yet </h3>
                     </div>
@@ -68,11 +85,24 @@ function EditSeriesPage(){
         });
         return(
             <div>
-                <button onClick={() => createEntry(maxOrder + 1)} type="submit"
-                        class="m-4 px-2 py-1 rounded-md text-slate-50 bg-orange-700 hover:bg-orange-800">
-                    Create New Entry
-                </button>
-                <div className="flex flex-col md:mx-52 flex flex-col my-3 space-y-0 mx-6">
+                <aside
+                    className="mt-10 fixed top-20 left-0 z-40 w-64 h-screen pt-10 transition-transform -translate-x-full sm:translate-x-0 bg-gradient-to-t from-gray-200">
+                    <div className="h-full px-3 overflow-y-auto">
+                        <ul className="space-y-2 font-medium">
+                            <li className="hover:bg-gray-200 rounded-xl p-4">
+                                <FontAwesomeIcon icon={faPenToSquare}/>
+                                <button class = "pl-2 text-xl" onClick={() => createEntry(maxOrder + 1)} type="submit">
+                                    Create New Entry
+                                </button>
+                            </li>
+                            <li className="hover:bg-gray-200 rounded-xl p-4">
+                                <FontAwesomeIcon icon={faCircleLeft} />
+                                <a className="pl-2 text-xl" href={writerUrl}>Back to Series Page</a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+                <div className="pl-20 flex flex-col md:mx-52 flex flex-col my-3 space-y-0 mx-6">
                     { entryItems }
                 </div>
             </div>
